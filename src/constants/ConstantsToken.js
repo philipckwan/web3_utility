db970ce9a60bc2c6de4d8111f14c7cb24bf2d99a
Philip Kwan <philipckwan@gmail.com>
2023-08-31 15:12:11 +0800

    update to ConstantsToken.js

--- src/constants/ConstantsToken.js
+++ src/constants/ConstantsToken.js
@@ -2,19 +2,19 @@ class ConstantsToken {
     static TOKENS_POLYGON_MAINNET = [
         ["0x2791bca1f2de4661ed88a30c99a7a9449aa84174", "USDC"],
         ["0xc2132d05d31c914a87c6611c10748aeb04b58e8f", "USDT"],
-        ["0x8f3cf7ad23cd3cadbd9735aff958023239c6a063", "DAI"],
-        ["0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6", "WBTC"],
-        ["0x53e0bca35ec356bd5dddfebbd1fc0fd03fabad39", "LINK"],
+        //["0x8f3cf7ad23cd3cadbd9735aff958023239c6a063", "DAI"],
+        //["0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6", "WBTC"],
+        //["0x53e0bca35ec356bd5dddfebbd1fc0fd03fabad39", "LINK"],
         ["0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270", "WMATIC"],
         ["0x7ceb23fd6bc0add59e62ac25578270cff1b9f619", "WETH"],
-        ["0xb33eaad8d922b1083446dc23f610c2567fb5180f", "UNI"],
+        //["0xb33eaad8d922b1083446dc23f610c2567fb5180f", "UNI"],
         ["0x16DFb898cf7029303c2376031392cb9baC450f94", "DMA"],
         ["0x831753DD7087CaC61aB5644b308642cc1c33Dc13", "QUICK"],
         ["0x0b3F868E0BE5597D5DB7fEB59E1CADBb0fdDa50a", "SUSHI"],
         //["0x7Ff54B5C384C9F3A3FeCE70e150D2Ce2D70DA6f7", "TBAC"],
         //["0xadBe0eac80F955363f4Ff47B0f70189093908c04", "XMT"],
         //["0xE2Aa7db6dA1dAE97C5f5C6914d285fBfCC32A128", "PAR"],
-        ["0xD6DF932A45C0f255f85145f286eA0b292B21C90B", "AAVE"],
+        //["0xD6DF932A45C0f255f85145f286eA0b292B21C90B", "AAVE"],
         //["0x82a0E6c02b91eC9f6ff943C0A933c03dBaa19689", "WNT"],
         //["0x516cdAe319F4B0E31A29c9572bc3F255679d7a0F", "DDD"],
         ["0xbbba073c31bf03b8acf7c28ef0738decf3695683", "SAND"],
@@ -28,6 +28,8 @@ class ConstantsToken {
         ["0xF79BF908d0e6d8E7054375CD80dD33424B1980bf", "mooBalancerMatic-stMaticV2"],
         ["0x3A58a54C066FdC0f2D55FC9C89F0415C92eBf3C4", "stMATIC"],
         ["0xC3C7d422809852031b44ab29EEC9F1EfF2A58756", "LDO"],
+        ["0xcdeDC76bcC6351275aCDeF823190453Ec43b4Edf", "xWMATIC-stMATIC01"],
+        ["0x8AAc0f2a6cAE626b656Ca16c58A50C87CBBC7ff5", "mooRetrostMATIC-MATIC"],
     ]
 
     static TOKENS_ETHEREUM_GOERLI = [
@@ -58,6 +60,8 @@ class ConstantsToken {
         ["0xc00e94Cb662C3520282E6f5717214004A7f26888", "COMP"],
         ["0x06325440D014e39736583c165C2963BA99fAf14E", "steCRV"],
         ["0xa7739fd3d12ac7F16D8329AF3Ee407e19De10D8D", "mooConvexStETH"],
+        ["0x5A98FcBEA516Cf06857215779Fd812CA3beF1B32", "LDO"],
+        ["", ""],
         ["", ""],
     ]
 
@@ -68,6 +72,8 @@ class ConstantsToken {
         ["0x841FAD6EAe12c286d1Fd18d1d525DFfA75C7EFFE", "BOO"],
         ["0xf0702249F4D3A25cD3DED7859a165693685Ab577", "spLP"],
         ["0xDE5ed76E7c05eC5e4572CfC88d1ACEA165109E44", "DEUS"],
+        ["0x321162Cd933E2Be498Cd2267a90534A804051b11", "BTC"],
+        ["0x04068DA6C83AFCFA0e13ba15A6696662335D5B75", "USDC"],
         ["", ""],
         ["", ""],
     ]
